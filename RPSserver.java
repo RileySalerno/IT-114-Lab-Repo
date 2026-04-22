1f680894f496ac7a38afcc01dd9e67e358cfffd4
Riley Salerno <rls38@njit.edu>
2026-04-21 21:23:26 -0400

    Created a way for The player to join the server and communicate on it

--- RPSserver.java
+++ RPSserver.java
@@ -16,4 +16,33 @@ public class RPSserver {
             new Thread(player).start();
         }
     }
+    
+    static class Player {
+        private Socket listener;
+        private BufferedReader incoming;
+        private PrintWriter outgoing;
+
+        public Player(Socket listener) throws Exception{
+            this.listener = listener;
+            incoming = new BufferedReader(new InputStreamReader(listener.getInputStream()));
+            outgoing = new PrintWriter(listener.getOutputStream(), true);
+        }
+
+         public void run() {
+            try {
+                outgoing.println("Enter 1 for Public Match, 2 to Create Private Room, 3 to Join Private Room:");
+                int roomType = Integer.parseInt(incoming.readLine());
+
+                if (roomType == 1) {
+                    publicMatch();
+                } else if (roomType == 2) {
+                    createPR();
+                } else if (roomType == 3) {
+                    joinPR();
+                }
+
+            } catch (Exception e) {
+                System.out.println("Client disconnected.");
+            }
+        }
 }
