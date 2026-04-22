48a096ccea5a3c71bb3429eeb01b4a58a6e0b07f
Riley Salerno <rls38@njit.edu>
2026-04-21 22:23:29 -0400

    Completed version of the Server that manages both the games Logic, and the creation of Rooms

--- RPSserver.java
+++ RPSserver.java
@@ -3,12 +3,12 @@ import java.util.*;
 import java.io.*;
 
 public class RPSserver {
-    private static List<Player> playerQueue = new ArrayList<>();
+    private static List<Player> queue = new ArrayList<>();
     private static Map<String, Player> privateRoom = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8080);
-        System.out.println("Server started");
+        System.out.println("Server has started");
 
         while (true) {
             Socket listener = serverSocket.accept();
@@ -16,8 +16,8 @@ public class RPSserver {
             new Thread(player).start();
         }
     }
-    
-    static class Player {
+
+    static class Player implements Runnable {
         private Socket listener;
         private BufferedReader incoming;
         private PrintWriter outgoing;
@@ -46,28 +46,6 @@ public class RPSserver {
             }
         }
 
-        private void createPR() throws Exception {
-            System.out.println("Enter a Password using only numbers or letters");
-            String roomPassword = incoming.readLine();
-
-            synchronized (privateRoom) {
-                privateRoom.put(roomPassword, this);
-                System.out.println("Private lobby successfully created. Password is: " + roomPassword);
-                System.out.println("Waiting for another Player to join the lobby");
-            }
-        }
-        private void joinPR() throws Exception {
-            System.out.println("Enter a Room Password to join");
-            String password = incoming.readLine();
-
-            if (privateRoom.containsKey(password)) {
-                Player host = privateRoom.remove(password);
-                new GameSession(host, this).start();
-            } else {
-                System.out.println("Room dosent exist.");
-            }
-        }
-
         private void publicMatch() throws Exception{
             synchronized(queue) {
                 queue.add(this);
@@ -82,4 +60,69 @@ public class RPSserver {
                 }
             }
         }
+        private void createPR() throws Exception {
+            System.out.println("Enter a Password using only numbers or letters");
+            String roomPassword = incoming.readLine();
+
+            synchronized (privateRoom) {
+                privateRoom.put(roomPassword, this);
+                System.out.println("Private lobby successfully created. Password is: " + roomPassword);
+                System.out.println("Waiting for another Player to join the lobby");
+            }
+        }
+
+        private void joinPR() throws Exception {
+            System.out.println("Enter a Room Password to join");
+            String password = incoming.readLine();
+
+            if (privateRoom.containsKey(password)) {
+                Player host = privateRoom.remove(password);
+                new GameSession(host, this).gameStart();
+            } else {
+                System.out.println("Room dosent exist.");
+            }
+        }
+
+        public int getMove() throws Exception{
+            System.out.println("Enter Rock(1), Paper(2), or Scissors(3)");
+            return Integer.parseInt(incoming.readLine());
+        }
+
+        public void send(String msg){
+            System.out.println(msg);
+        }
+
+        static class GameSession {
+            private Player p1;
+            private Player p2;
+
+            public GameSession(Player p1,Player p2) {
+                this.p1 = p1;
+                this.p2 = p2;
+            }
+
+            public void gameStart() throws Exception {
+                p1.send("Matched with: ");
+                p2.send("Matched with: ");
+
+                int move1 = p1.getMove();
+                int move2 = p2.getMove();
+
+                String result = getResult(move1, move2);
+
+                p1.send(result);
+                p2.send(result);
+            }
+
+            private String getResult(int p1, int p2) {
+                if (p1 == p2) return "Tie";
+            if ((p1 == 1 && p2 == 3) ||
+                (p1 == 2 && p2 == 1) ||
+                (p1 == 3 && p2 == 2)) {
+                return "Player 1 wins";
+            }
+            return "Player 2 wins";
+            }
+        }
+    }
 }
