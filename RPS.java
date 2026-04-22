69764f0a17e17bd3a97c7a1df4129aaabc06f709
Riley Salerno <rls38@njit.edu>
2026-04-21 22:48:41 -0400

    Updated RPS to handle both the game flow and the players
    Refactor RPS class to use Player objects and handle game logic.

--- RPS.java
+++ RPS.java
@@ -1,13 +1,31 @@
-public class RPS{
-    public static String getResult(int p1, int p2){
-        if (p1 == p2) {
-            System.out.println("Tie");
-        } else if ( (p1 == 1 && p2 == 3) || (p1 == 2 && p2 == 1) || (p1 == 3 && p2 == 2)) {
-            System.out.println("Player 1 wins");
-        } 
-        else {
-            System.out.println("Player 2 wins");
-        }
+public class RPS {
+    private Player p1;
+    private Player p2;
 
+    public RPS(Player p1,Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public void gameStart() throws Exception {
+        p1.send("Matched with: ");
+        p2.send("Matched with: ");
+
+        int move1 = p1.getMove();
+        int move2 = p2.getMove();
+
+        String result = getResult(move1, move2);
+
+        p1.send(result);
+        p2.send(result);
+    }
+
+    private String getResult(int p1, int p2) {
+        if (p1 == p2) return "Tie";
+
+        if ((p1 == 1 && p2 == 3) || (p1 == 2 && p2 == 1) || (p1 == 3 && p2 == 2)) {
+            return "Player 1 wins";
+        }
+        return "Player 2 wins";
     }
 }
