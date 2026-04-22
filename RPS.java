8988c571a5d93c1afdc4f834afc6053070c872c3
Riley Salerno <rls38@njit.edu>
2026-04-21 22:29:14 -0400

    Changed RPS to just be the results of the game

--- RPS.java
+++ RPS.java
@@ -1,18 +1,6 @@
-import java.util.Scanner;
-
 public class RPS{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Player 1 - Enter (1) for Rock, (2) for Paper, or (3) for Scissors");
-        int p1 = scan.nextInt();
-
-        System.out.print("Player 2 - Enter (1) for Rock, (2) for Paper, or (3) for Scissors");
-        int p2 = scan.nextInt();
-
-        if (p1 < 1 || p1 > 3 || p2 < 1 || p2 > 3) {
-            System.out.println("Please enter 1, 2, or 3");
-        } else if (p1 == p2) {
+    public static String getResult(int p1, int p2){
+        if (p1 == p2) {
             System.out.println("Tie");
         } else if ( (p1 == 1 && p2 == 3) || (p1 == 2 && p2 == 1) || (p1 == 3 && p2 == 2)) {
             System.out.println("Player 1 wins");
@@ -21,6 +9,5 @@ public class RPS{
             System.out.println("Player 2 wins");
         }
 
-        scan.close();
     }
 }
