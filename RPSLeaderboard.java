052f0742cbe5b2deec318eb7851cb21c6c0fea4f
Riley Salerno <rls38@njit.edu>
2026-05-10 12:38:01 -0400

    Added methods to save and load leaderboard

---
+++ RPSLeaderboard.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class RPSLeaderboard {
+    public static Map<String, Integer> leaderboard = new HashMap<>();
+
+    public static void loadBoard() {
+        File file = new File("leaderboard.txt");
+
+        if(!file.exists()) return;
+
+        try (Scanner scan = new Scanner(file)){
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+
+                String[] parts = line.split(":");
+
+                leaderboard.put(parts[0],Integer.parseInt(parts[1]));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveBoard() {
+        try (PrintWriter writer = new PrintWriter("leaderboard.txt")){
+            for (Map.Entry<String, Integer> entry : leaderboard.entrySet()){
+                writer.println(entry.getKey() + ":" + entry.getValue());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
