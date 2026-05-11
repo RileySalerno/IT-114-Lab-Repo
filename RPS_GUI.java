c45c10e6890b9f3f7fae042809af83ad681e365e
Riley Salerno <rls38@njit.edu>
2026-05-11 00:24:57 -0400

    added main method, set up for states and a start method
    Added main method and GUI setup for Rock Paper Scissors game.

--- RPS_GUI.java
+++ RPS_GUI.java
@@ -20,10 +20,48 @@ public class RPS_GUI {
     private JTextField passwordField;
     private int pendingAction;
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new RPS_GUI().start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    public void start() throws Exception {
+        setupConnection();
+        setupGUI();
+        startListenerThread();
+    }
+
      private void setupConnection() throws Exception {
         Socket socket = new Socket("localhost", 8080);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    private void setupGUI() {
+        frame = new JFrame("Rock Paper Scissors");
+        cards = new CardLayout();
+        mainPanel = new JPanel(cards);
+
+        mainPanel.add(loginPanel(), "login");
+        mainPanel.add(menuPanel(), "menu");
+        mainPanel.add(gamePanel(), "game");
+        mainPanel.add(waitingPanel(), "waiting");
+        mainPanel.add(waitingMovePanel(), "waitingMove");
+        mainPanel.add(resultPanel(), "result");
+        mainPanel.add(passwordPanel(), "password");
+
+        frame.add(mainPanel);
+        frame.setSize(420, 260);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        cards.show(mainPanel, "login");
     }
 
 
